6aa0c9f6fab7c5f548247a0ad8480951aa7115eb
Jonathan Hall <flimzy@flimzy.com>
2019-01-22 18:32:03 +0100

    Add simple File reader type

---
+++ file.go
@@ -0,0 +1,30 @@
+package diff
+
+import (
+	"io"
+	"os"
+)
+
+// File converts a file into an io.Reader.
+type File struct {
+	Path string
+	r    io.Reader
+	done bool
+}
+
+var _ io.Reader = &File{}
+
+func (f *File) Read(p []byte) (int, error) {
+	if f.done {
+		return 0, io.EOF
+	}
+	if f.r == nil {
+		var err error
+		if f.r, err = os.Open(f.Path); err != nil {
+			return 0, err
+		}
+	}
+	n, err := f.r.Read(p)
+	f.done = err != nil
+	return n, err
+}
